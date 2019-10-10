d29c052b5af10e053769c9b6e73204f03a5e0eb7
Joeli Takala <joeli.takala@helsinki.fi>
2019-10-10 13:02:01 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Skosmos-widget-message
+
+This is a general-purpose notification widget for Skosmos. The message strings should be passed to Skosmos front-end as an associative js-array named 'pluginMessages' with ISO 639-1 language codes as keys.
---
+++ message-plugin.js
@@ -0,0 +1,25 @@
+// declaring a namespace for the plugin
+var MESSAGE = MESSAGE || {};
+
+MESSAGE = {
+    notice: {
+      'fi': 'HUOM!',
+      'sv': 'OBS!',
+      'en': 'NB:'
+    },
+    renderWarning: function(params) {
+        $('.content').prepend(Handlebars.compile($('#message-template').html())({
+		'notice': this.notice[lang],
+		'message': pluginMessages[lang]
+        }));
+    },
+};
+
+$(function() {
+    window.displayMessage = function(params) {
+        if (params.page === 'page' && (params.uri  || $('#vocab-info').length)) {
+            MESSAGE.renderWarning(params);
+        }
+    };
+});
+
