7773559c69fd5dd5875f838ef2aaa839785897f7
kouralex <1723419+kouralex@users.noreply.github.com>
2020-12-04 15:20:40 +0200

    update the plugin to be more versatile

--- message-plugin.js
+++ message-plugin.js
@@ -2,21 +2,25 @@
 var MSG = MSG || {};
 
 MSG = {
-    notice: {
-      'fi': 'HUOM!',
-      'sv': 'OBS!',
-      'en': 'NB:'
-    },
+    supportedParams: ['msg', 'cssClasses', 'id'],
     renderWarning: function(params) {
-        var msg = 'msg';
-        if (pluginParameters['message']['msg_'+lang] != null) {
-            msg = 'msg_'+lang;
+        var supportedParamsLength = this.supportedParams.length;
+        var contextDict = {};
+        for (var i = 0; i < supportedParamsLength; i++) {
+            var key = this.supportedParams[i];
+            // assumes that this widget is called "message"
+            if (pluginParameters['message'][key + '_' +lang] != null) {
+                contextDict[key] = pluginParameters['message'][key + '_' + lang];
+                continue;
+            }
+            if (pluginParameters['message'][key] != null) {
+                contextDict[key] = pluginParameters['message'][key];
+                continue;
+            }
+            contextDict[key] = "";
         }
 
-        $('.content').prepend(Handlebars.compile($('#message-template').html())({
-            'notice': this.notice[lang],
-            'message': pluginParameters['message'][msg]
-        }));
+        $('.content').prepend(Handlebars.compile($('#message-template').html())(contextDict));
     },
 };
 
